48bdd971bde0569dd882321f1eb9535ae6c3801f
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-07-03 18:39:14 +0530

    modified:   src/components/SoundSection.jsx 	modified:   src/components/WebViewer.jsx 	modified:   src/index.css 	new file:   src/lib/scroll-animation.js

--- src/components/SoundSection.jsx
+++ src/components/SoundSection.jsx
@@ -2,6 +2,16 @@
 import React from 'react';
 
 function SoundSection() {
+
+    const handleLearnMore = () => {
+        const element = document.querySelector(".display-section");
+        window.scrollTo({
+            top: element?.getBoundingClientRect().bottom,
+            left: 0,
+            behavior: "smooth"
+        })
+    }
+
     return (
         <div className='sound-section wrapper'>
             <div className='body'>
@@ -14,7 +24,7 @@ function SoundSection() {
                     <ul className='links'>
                         <li><button className='button'>Buy</button></li>
                         <li>
-                            <a className='link'>Learn more</a>
+                            <a className='link' onClick={handleLearnMore}>Learn more</a>
                         </li>
                     </ul>
                 </div>
